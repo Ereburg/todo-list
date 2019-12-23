2199bda4ea5f35bf172d0c7c8ee57e8bcaacc074
Eugene <eugene.kotsarev@gmail.com>
2019-12-23 03:09:10 +0300

    update 1.1 migrate to newer gulp version, mobile adaptive fix.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,15 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const autoprefixer = require("gulp-autoprefixer");
-const browserSync = require("browser-sync").create();
+global.$ = {
+    gulp: require('gulp'),
+    plugins: require('gulp-load-plugins')(), // плагин, позволяющий не прописывать каждый плагин как переменную
+    bs: require('browser-sync').create(), // онлайн-сервер
 
-const sassOptions = {
-  outputStyle: "expanded"
+    path: {
+        tasks: require('./gulp/config/tasks.js') // путь к конфигу, где прописаны в котором собран массив из путей к задачам галпа
+    }
 };
 
-// compile scss into css
+$.path.tasks.forEach((taskPath) => {
+    require(taskPath)(); // собираем задачи
+});
 
-function style() {
-  return gulp
-    .src("./scss/**/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on("error", sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(autoprefixer())
-    .pipe(gulp.dest("./css"))
-    .pipe(browserSync.stream());
-}
-
-function watch() {
-  browserSync.init({
-    server: "."
-  });
-  gulp.watch("./scss/**/*.scss", style);
-  gulp.watch("./*.html").on("change", browserSync.reload);
-  gulp.watch("./js/**/*.js", browserSync.reload);
-}
-
-exports.style = style;
-exports.watch = watch;
+$.gulp.task('default', $.gulp.parallel('html', 'styles', 'scripts', 'server')); // команда по умолчанию
